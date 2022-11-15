1fe7972674fd3401fa2d3ed2a10e3d228778a467
NitinEkka <103876655+NitinEkka@users.noreply.github.com>
2022-11-15 15:53:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from urllib.request import urlopen
+from geopy.geocoders import ArcGIS
 
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 ipAdd = str(ip_request.json()['ip'])
@@ -9,4 +10,12 @@ url = 'http://api.db-ip.com/v2/free/'+ipAdd
 response = urlopen(url)
 data = json.load(response)
 print(ipAdd)
-print(data['city'])
+city = data['city']
+nom = ArcGIS()
+city_coor = nom.geocode(city)
+print(city_coor)
+splitted = city_coor[0].split(", ")
+max_len = int(len(splitted))
+index = max_len - 2
+city_name = splitted[index]
+print(city_name)
